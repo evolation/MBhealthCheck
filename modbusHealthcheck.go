3c59a14462ab08e12a3d6a055917381720667b25
George Nakas <george.nakas@paul-digital.de>
2023-08-08 13:27:28 +0200

    add rows without focus

--- modbusHealthcheck.go
+++ modbusHealthcheck.go
@@ -1,19 +1,24 @@
 package main
 
-import "github.com/maxence-charriere/go-app/v9/pkg/app"
+import (
+	"fmt"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
 
 type tablePage struct {
 	app.Compo
-
-	Rows [][]string
+	Rows        [][]string
+	RowToFocus  int
+	ShouldFocus bool
 }
 
 func (t *tablePage) Render() app.UI {
 	headers := []app.UI{
 		app.Tr().Body(
-			app.Th().Text("Column 1"),
-			app.Th().Text("Column 2"),
-			app.Th().Text("Column 3"),
+			app.Th().Text("MB Address"),
+			app.Th().Text("Status"),
+			app.Th().Text("Result"),
 		),
 	}
 
@@ -29,11 +34,38 @@ func (t *tablePage) Render() app.UI {
 	)
 }
 
+func (t *tablePage) OnInputChange(row []string) func(ctx app.Context, e app.Event) {
+	return func(ctx app.Context, e app.Event) {
+		newValue := ctx.JSSrc().Get("value").String()
+		row[0] = newValue
+		t.Update()
+	}
+}
+
+func (t *tablePage) OnInputKeyDown(ctx app.Context, e app.Event) {
+	keyCode := e.Get("keyCode").Int()
+	if keyCode == 13 { // 13 is the key code for Enter
+		t.Rows = append(t.Rows, []string{"", "New Data 2", "New Data 3"})
+		t.RowToFocus = len(t.Rows) - 1
+		t.ShouldFocus = true
+		t.Update()
+	}
+}
+
 func (t *tablePage) renderRows() []app.UI {
 	var uiRows []app.UI
-	for _, row := range t.Rows {
+	for i, row := range t.Rows {
+		inputID := fmt.Sprintf("input-row-%d", i)
+		fmt.Printf("inputID: %s\n", inputID)
+		inputElem := app.Input().
+			Type("text").
+			Value(row[0]).
+			OnChange(t.OnInputChange(row)).
+			OnKeyDown(t.OnInputKeyDown).
+			ID(inputID)
+
 		uiRow := app.Tr().Body(
-			app.Td().Text(row[0]),
+			app.Td().Body(inputElem),
 			app.Td().Text(row[1]),
 			app.Td().Text(row[2]),
 		)
@@ -43,7 +75,8 @@ func (t *tablePage) renderRows() []app.UI {
 }
 
 func (t *tablePage) OnAddRowClick(ctx app.Context, e app.Event) {
-	t.Rows = append(t.Rows, []string{"New Data 1", "New Data 2", "New Data 3"})
+	fmt.Println("OnAddRowClick triggered") // Add this line
+	t.Rows = append(t.Rows, []string{"", "Pending", "Pending"})
 	t.Update()
 }
 
